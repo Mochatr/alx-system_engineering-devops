7c2ba9da20328be1251c2b40f426b9801ded4234
Mochatr <mohamedchatr8@gmail.com>
2024-03-05 09:15:39 -0800

    Update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,27 +8,19 @@ def number_of_subscribers(subreddit):
     Queries the Reddit API to get the number of subscribers
     for a given subreddit
 
-    Parameters:
-    subreddit(str): The name of the subreddit.
+    Args:
+        subreddit(str): The name of the subreddit.
 
     Returns:
-    int: The number of subscribers for the subreddit.
-    Return 0 if the subreddit is invalid.
+        int: The number of subscribers for the subreddit.
+        Return 0 if the subreddit is invalid.
     """
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'custom user-agent'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
-        if 'data' in data and 'subscribers' in data['data']:
-            return data['data']['subscribers']
-    return 0
+    if response.status_code == 404:
+        return 0
 
-
-if __name__ == "__main__":
-    # Example of a subreddit.
-    subreddit = "Blockchain"
-    subscribers = number_of_subscribers(subreddit)
-    print(f"The subreddit '{subreddit}' has {subscribers} subscribers.")
+    return response.json().get('data', {}).get('subscribers', 0)
