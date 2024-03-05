fa5a77a39936e224119394083e3639932df6dc50
Mochatr <mohamedchatr8@gmail.com>
2024-03-05 08:06:36 -0800

    Task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API to get the titles of the first 10 hot posts
+    for a given subreddit
+    """
+
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'User-Agent': 'custom user-agent'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data and 'children' data['data']:
+            for post in data['data']['children']:
+                print(post['data']['title'])
+        else:
+            print(None)
+    else:
+        print(None)
+
+
+if __name__ == "__main__":
+    # Example of a subreddit
+    subreddit = "Gaming"
+    top_ten(subreddit)
