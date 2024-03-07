f27ef6fd2a9da281392f6ef366cbf41267a2b751
Mochatr <mohamedchatr8@gmail.com>
2024-03-07 12:53:30 -0800

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# This manifest fixes the Apache 500 error by setting file permissions
-
-exec {
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
-}
